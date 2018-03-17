ea0132f67edc12360857940b0140ba3e1c11d841
GAUCON <GAUCON@DESKTOP-62S7648>
2018-03-17 11:25:52 +0700

    thử 1 vài test case

--- src/main/java/media/testjenkins/MathClass.java
+++ src/main/java/media/testjenkins/MathClass.java
@@ -2,7 +2,10 @@ package media.testjenkins;
 
 public class MathClass {
 
-    public int plus(int a, int b) {
+    public int plus(int a, int b) throws Exception {
+        if (a < 0 || b < 0) {
+            throw new Exception("Số truyền vào phải dương");
+        }
         return a + b;
     }
 }
--- src/test/java/test1/MathJUnitTest.java
+++ src/test/java/test1/MathJUnitTest.java
@@ -7,6 +7,8 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
 
 public class MathJUnitTest {
 
@@ -33,14 +35,25 @@ public class MathJUnitTest {
     // The methods must be annotated with annotation @Test. For example:
     //
     @Test
-    public void testPlus() {
+    public void testPlus() throws Exception {
         media.testjenkins.MathClass c = new MathClass();
         assertEquals("2 + 2 phai bang 4 moi dung", 4, c.plus(2, 2));
     }
-    
+
     @Test
-    public void testPlus2() {
+    public void testPlus2() throws Exception {
         media.testjenkins.MathClass c = new MathClass();
         assertEquals("3 + 2 phai bang 5 moi dung", 5, c.plus(3, 2));
+    }
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    @Test
+    public void testParameterNegative() throws Exception{
+        exception.expect(Exception.class);
+        exception.expectMessage("Số truyền vào phải dương");
+        MathClass t = new MathClass();
+        t.plus(-1, -2);
     }
 }
