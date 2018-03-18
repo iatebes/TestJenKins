9799ee93ce61fceb2dded44b110a9bc08fa8f28a
iatebes <clgt@outlook.com>
2018-03-18 23:07:29 +0700

    Thử dùng selenium để test trên GUI, kết quả phải deploy web lên URL thì mới có thể test bằng Selenium được

--- src/test/java/test1/MathTestNGTest.java
+++ src/test/java/test1/MathTestNGTest.java
@@ -1,10 +1,15 @@
 package test1;
 
 import media.testjenkins.MathClass;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import static org.testng.Assert.*;
 import org.testng.annotations.Test;
 
 public class MathTestNGTest {
+    
+    private static WebDriver driver;
     
     public MathTestNGTest() {
     }
@@ -17,10 +22,13 @@ public class MathTestNGTest {
 
     @org.testng.annotations.BeforeClass
     public static void setUpClass() throws Exception {
+        System.setProperty("webdriver.gecko.driver", "C:\\WebDriver\\geckodriver-v0.20.0-win64\\geckodriver.exe");
+        driver = new FirefoxDriver();
     }
 
     @org.testng.annotations.AfterClass
     public static void tearDownClass() throws Exception {
+        driver.close();
     }
 
     @org.testng.annotations.BeforeMethod
@@ -47,5 +55,27 @@ public class MathTestNGTest {
     public void testParameterNegative() throws Exception {
         MathClass t = new MathClass();
         t.plus(-1, -2);
+    }
+    
+    @Test
+    public void testGUI() {
+        // Mo trang web
+        driver.get("http://118.70.183.219:8080/TestJenKins/");
+        
+        // nhap so 1
+        driver.findElement(By.id("so1")).sendKeys("6");
+        String so1 = driver.findElement(By.id("so1")).getAttribute("value");
+        
+        // nhap so 2
+        driver.findElement(By.id("so2")).sendKeys("7");
+        String so2 = driver.findElement(By.id("so2")).getAttribute("value");
+        
+        // an nut cong
+        driver.findElement(By.id("btnCong")).click();
+        
+        // doc ket qua
+        String ketqua = driver.findElement(By.xpath(".//*[@id='lblketqua']")).getText();
+//        System.out.println("so1:" + so1 + ", so2:" + so2 + ", ketqua:" + ketqua);
+        assertEquals("13", ketqua);
     }
 }
